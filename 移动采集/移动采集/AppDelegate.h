217f74c3ce8de83a4cf1ca94d30af8965c21195f
hcat <hcat@MACdeMac-mini.local>
2017-07-17 11:12:38 +0800

    提交工程

---
+++ 移动采集/移动采集/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  移动采集
+//
+//  Created by hcat on 2017/7/17.
+//  Copyright © 2017年 Hcat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 移动采集/移动采集/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  移动采集
+//
+//  Created by hcat on 2017/7/17.
+//  Copyright © 2017年 Hcat. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
