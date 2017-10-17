8f680a45883cd6148bd049fb3a1148f0cb1913c1
hcat <hcat@MACdeMac-mini.local>
2017-10-17 17:14:11 +0800

    提交版本

--- 移动采集/移动采集/Models/VehicleModel.h
+++ 移动采集/移动采集/Models/VehicleModel.h
@@ -17,12 +17,13 @@
 @property (nonatomic,strong) NSNumber * bussInsuranceTimeEnd;       //商业险截止日期
 @property (nonatomic,copy)   NSString * factoryno;                  //车架号码
 @property (nonatomic,copy)   NSString * motorid;                    //发动机号码
-@property (nonatomic,copy)   NSString * description_text;                //车辆描述
+@property (nonatomic,copy)   NSString * description_text;           //车辆描述
 @property (nonatomic,copy)   NSString * driver;                     //车主姓名
 @property (nonatomic,copy)   NSString * dvrcard;                    //车主身份证
 @property (nonatomic,copy)   NSString * drivermobile;               //车主电话
 @property (nonatomic,strong) NSNumber * status;                     //车辆状态 1正常 0暂停运营 2停止运营 3未审核 4未提交 5审核未通过
 @property (nonatomic,copy)   NSString * remark;                     //备注
+@property (nonatomic,copy)   NSString * carriageOutsideH;          //车厢外高度
 @property (nonatomic,copy)   NSString * carHopperL;                 //车斗信息（长）
 @property (nonatomic,copy)   NSString * carHopperW;                 //车斗信息（宽）
 @property (nonatomic,copy)   NSString * carHopperH;                 //车斗信息（高）
