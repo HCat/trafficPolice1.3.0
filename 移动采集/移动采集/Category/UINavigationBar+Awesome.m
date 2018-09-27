d5c8eae1bb6d35b241b42fdee137aab0f304e582
hcat <>
2018-09-27 20:50:53 +0800

    提交代码

--- 移动采集/移动采集/Category/UINavigationBar+Awesome.m
+++ 移动采集/移动采集/Category/UINavigationBar+Awesome.m
@@ -36,7 +36,9 @@ static char emptyImageKey;
 
 - (void)lt_setBackgroundColor:(UIColor *)backgroundColor
 {
-   
+    [self.overlay removeFromSuperview];
+    self.overlay = nil;
+    
     if (!self.overlay) {
         [self setBackgroundImage:[UIImage new] forBarMetrics:UIBarMetricsDefault];
         
